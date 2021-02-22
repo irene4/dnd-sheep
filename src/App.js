4c3ea97a79f6adf8e6a7c7d38b4ba4c72ffae949
irene4 <iallman@ufl.edu>
2021-02-21 22:34:32 -0500

    Stats grid spacing and placeholder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './styles/App.css';
-import Card from './Card';
+import Card from './components/Card';
 
 function App() {
 	return (
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ function Card() {
 		<div className={`flex h-60 rounded-2xl bg-${bgColor}-200 p-4 shadow`}>
 			<div className="">
 				<img
-					className="p-1 rounded-full w-5/6"
+					className="rounded-full w-5/6 shadow-md"
 					src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.15752-9/151467208_145327264098759_5141235477982174585_n.png?_nc_cat=111&ccb=3&_nc_sid=ae9488&_nc_ohc=xZko6rj2vgAAX8mhZC0&_nc_ht=scontent-atl3-1.xx&oh=2373c3749fa11cac51b880a65317de78&oe=60570172"
 				/>
 			</div>
@@ -24,19 +24,19 @@ function Card() {
 						className="text-xs text-gray-600 italic w-full rounded bg-transparent pl-1"
 						placeholder="✎ Alignment..."
 					></input>
-					<div className="grid grid-cols-4 sm:grid-cols-6 items-center">
+					<div className="grid grid-cols-4 sm:grid-cols-6 items-center w-48 sm:w-72">
 						<label className="text-gray-500 font-bold tracking-wider uppercase text-xs p-1">STR</label>
-						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="#"></input>
+						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="# (±#)"></input>
 						<label className="text-gray-500 font-bold tracking-wider uppercase text-xs p-1">DEX</label>
-						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="#"></input>
+						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="# (±#)"></input>
 						<label className="text-gray-500 font-bold tracking-wider uppercase text-xs p-1">CON</label>
-						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="#"></input>
+						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="# (±#)"></input>
 						<label className="text-gray-500 font-bold tracking-wider uppercase text-xs p-1">INT</label>
-						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="#"></input>
+						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="# (±#)"></input>
 						<label className="text-gray-500 font-bold tracking-wider uppercase text-xs p-1">WIS</label>
-						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="#"></input>
+						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="# (±#)"></input>
 						<label className="text-gray-500 font-bold tracking-wider uppercase text-xs p-1">CHA</label>
-						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="#"></input>
+						<input type="text" className="w-16 rounded p-1 bg-transparent" placeholder="# (±#)"></input>
 					</div>
 				</div>
 			</div>
