d3adcb9959486f24efb91a3535e77ec99e3f1c6b
irene4 <iallman@ufl.edu>
2021-02-25 16:40:52 -0500

    Image url update input

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Dice from './Dice';
 import useFirestore from '../hooks/useFirestore';
 
@@ -15,19 +16,36 @@ function Card({ data, slug }) {
 	return (
 		<div className={`flex h-72 w-full sm:w-1/2 md:w-1/3 lg:w-3/10 sm:rounded-2xl bg-white p-4 pr-8 sm:m-2 sm:shadow-md`}>
 			<div className="flex flex-col items-center">
-				<img className="rounded-full shadow-md w-5/6 m-1" src={data.url || nopicUrl} alt="Character" />
+				<div className="relative max-w-10">
+					<img className="rounded-full shadow-md w-5/6 m-1" src={data.url || nopicUrl} alt="Character" />
+					<input
+						id="url"
+						value={data.url}
+						type="text"
+						className="absolute bottom-1/4 left-1/4 border text-blue-400 rounded shadow"
+						placeholder="Paste an image url here!"
+						onChange={update}
+					/>
+				</div>
 				<span className="flex items-center">
 					<label className="text-gray-600 font-bold tracking-wider uppercase text-xs">HP</label>
 					<input
 						id="hp.curr"
-						value={data.hp.curr}
+						value={data.hp.curr || '#'}
 						type="text"
 						className="font-bold rounded bg-transparent w-6 text-right"
 						placeholder="#"
 						onChange={update}
 					/>
 					<span className="px-1">/</span>
-					<input id="hp.total" value={data.hp.total} type="text" className="rounded bg-transparent w-6" placeholder="#" onChange={update} />
+					<input
+						id="hp.total"
+						value={data.hp.total || '#'}
+						type="text"
+						className="rounded bg-transparent w-6"
+						placeholder="#"
+						onChange={update}
+					/>
 				</span>
 				<Dice data={data.dice || ''} update={update} updateDice={updateDice} />
 				<div className="text-left mt-1">
