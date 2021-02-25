6f7bb4e80591206cf0ef59773af94ca0146f5494
irene4 <iallman@ufl.edu>
2021-02-25 15:46:32 -0500

    useFirestore hook now has updateField and createPlayer fxns

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -14,7 +14,45 @@ const useFirestore = (room) => {
 		});
 		return () => cleanup();
 	}, [room]);
-	return { roomContent };
+
+	function createPlayer() {
+		db.collection(room)
+			.doc()
+			.set({ ...newFields, type: 'player', active: true });
+	}
+
+	function updateField(id, field, value) {
+		let newValue = {};
+		newValue[field] = value;
+		db.collection(room).doc(id).update(newValue);
+	}
+
+	return { roomContent, createPlayer, updateField };
+};
+
+const newFields = {
+	name: '',
+	class: '',
+	alignment: '',
+	stats: {
+		str: '',
+		dex: '',
+		con: '',
+		int: '',
+		wis: '',
+		cha: '',
+	},
+	actions: '',
+	notes: '',
+	hp: {
+		total: 0,
+		curr: 0,
+	},
+	dice: {
+		lastRoll: 0,
+		sides: 20,
+	},
+	url: '',
 };
 
 export default useFirestore;
