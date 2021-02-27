5844fcb818b1ed1dcc4ae6b8aef38ba4822d6b13
irene4 <iallman@ufl.edu>
2021-02-26 19:36:55 -0500

    Removed hp and added status

--- src/components/Enemy.js
+++ src/components/Enemy.js
@@ -12,35 +12,23 @@ function Enemy({ data, slug }) {
 	}
 
 	return (
-		<div className="flex sm:flex-col relative h-20 sm:h-32 w-full sm:w-36 sm:rounded-2xl bg-red-500 items-center p-4 sm:m-2 sm:shadow-md">
+		<div className="flex sm:flex-col relative w-full sm:w-36 sm:rounded-2xl bg-red-500 items-center p-4 sm:m-2 sm:shadow-md">
 			<input
 				id="name"
 				value={data.name}
 				type="text"
-				className="w-full text-2xl text-yellow-200 font-bold rounded bg-transparent pl-1 overflow-ellipsis"
+				className="w-full text-xl text-yellow-200 font-bold rounded bg-transparent pl-1 overflow-ellipsis"
 				placeholder="✎ Enemy..."
 				onChange={update}
 			/>
-			<span className="flex items-center">
-				<label className="text-yellow-200 font-bold tracking-wider uppercase text-xs">HP</label>
-				<input
-					id="hp.curr"
-					value={data.hp.curr || '#'}
-					type="text"
-					className="font-bold rounded bg-transparent w-6 text-right text-yellow-200"
-					placeholder="#"
-					onChange={update}
-				/>
-				<span className="px-1 text-white">/</span>
-				<input
-					id="hp.total"
-					value={data.hp.total || '#'}
-					type="text"
-					className="rounded bg-transparent w-6 text-yellow-200"
-					placeholder="#"
-					onChange={update}
-				/>
-			</span>
+			<input
+				className="w-full text-yellow-200 rounded bg-transparent mb-1 text-center"
+				id="status"
+				value={data.status}
+				type="text"
+				placeholder="Status"
+				onChange={update}
+			/>
 			<span className="pl-1 pr-2 bg-white rounded">
 				<Dice data={data.dice || ''} update={update} updateDice={updateDice} />
 			</span>
