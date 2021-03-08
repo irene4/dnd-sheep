0e3da56750e76ec087fdf08a9974b8500880cb93
irene4 <iallman@ufl.edu>
2021-03-07 19:36:49 -0500

    Import CodeInput component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/App.css';
 import Cards from './components/Cards';
+import CodeInput from './components/CodeInput';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 				<Switch>
 					<Route exact path="/">
 						<h1 className="m-1 text-xl">Join a room by adding its /[Room Name] to the URL!</h1>
+						<CodeInput />
 					</Route>
 					<Route path="/:slug">
 						<Cards />
