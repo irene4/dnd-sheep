faa45cd006cb94184ccfaa29f1cdd0aa0285101f
irene4 <iallman@ufl.edu>
2021-03-07 19:37:38 -0500

    CodeInput component

---
+++ src/components/CodeInput.js
@@ -0,0 +1,9 @@
+function CodeInput() {
+	return (
+		<div className="flex h-full w-full items-center justify-center">
+			<input className="p-3 text-3xl rounded-lg w-64" placeholder="Room code..." />
+		</div>
+	);
+}
+
+export default CodeInput;
