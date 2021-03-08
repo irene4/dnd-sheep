bfb02d0b2b4aa82ec428ddad3000b743efb0efe4
irene4 <iallman@ufl.edu>
2021-03-07 21:01:13 -0500

    input positioning

--- src/components/CodeInput.js
+++ src/components/CodeInput.js
@@ -1,7 +1,8 @@
 function CodeInput() {
 	return (
-		<div className="flex h-full w-full items-center justify-center">
-			<input className="p-3 text-3xl rounded-lg w-64" placeholder="Room code..." />
+		<div className="flex flex-col h-full w-full items-center justify-around">
+			<input className="p-3 text-blue-400 text-4xl rounded-lg w-64" placeholder="Room code..." />
+			<div></div>
 		</div>
 	);
 }
