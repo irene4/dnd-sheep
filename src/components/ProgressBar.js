0fccb9bfb93d0b5c9219bd9fef7602c3aab317ca
irene4 <iallman@ufl.edu>
2021-03-22 23:09:23 -0400

    Progress bar component

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,15 @@
+function ProgressBar({ curr, tot }) {
+	function colorSolver() {
+		if (curr / tot < 0.3) return 'red';
+		else if (curr / tot < 0.7) return 'yellow';
+		else if (curr / tot <= 1) return 'green';
+		else return 'white';
+	}
+	return (
+		<div className="w-1/2 border rounded">
+			<div className={`bg-${colorSolver()}-500 py-1 rounded`} style={{ width: `${(curr / tot) * 100}%`, maxWidth: '100%' }}></div>
+		</div>
+	);
+}
+
+export default ProgressBar;
