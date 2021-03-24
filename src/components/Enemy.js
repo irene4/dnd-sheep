4bea295730b3f2251e79e078b683c504f7ed6547
irene4 <iallman@ufl.edu>
2021-03-24 12:44:14 -0400

    Replace X with icon

--- src/components/Enemy.js
+++ src/components/Enemy.js
@@ -32,8 +32,14 @@ function Enemy({ data, slug }) {
 			<span className="pl-1 pr-2 bg-white rounded">
 				<Dice data={data.dice || ''} update={update} updateDice={updateDice} />
 			</span>
-			<button className="absolute text-sm text-yellow-200 top-2 right-4" onClick={() => updateField(data.id, 'active', false)}>
-				x
+			<button className="absolute text-sm text-yellow-200 top-2 right-2" onClick={() => updateField(data.id, 'active', false)}>
+				<svg className="w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+					<path
+						fillRule="evenodd"
+						d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+						clipRule="evenodd"
+					/>
+				</svg>
 			</button>
 		</div>
 	);
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,4 +1,5 @@
 function Tooltip({ text, sz }) {
+	// sz should be a Tailwind value like xs, sm, xl, etc.
 	return <div className={`tooltip p-1 text-${sz} rounded`}>{text}</div>;
 }
 
