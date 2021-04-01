8aa7076ef12b52e023419dd4b57218c20be0b274
irene4 <iallman@ufl.edu>
2021-04-01 02:12:48 -0400

    Changed bg color picker button/icon

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -11,13 +11,23 @@ function ColorPicker({ data, updateField }) {
 		<div className="relative m-2">
 			<div className="flex">
 				<button
-					className="w-10 h-10 border border-black rounded-full focus:outline-none focus:shadow-outline inline-flex p-2 shadow"
+					className="h-10 w-10 text-white hover:text-gray-200 rounded-full focus:outline-none focus:shadow-outline inline-flex p-2"
 					style={{ background: `${data.backgroundColor}` }}
 					onClick={() => setIsOpen(!isOpen)}
 				>
-					<svg className="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-						<path fill="none" d="M15.584 10.001L13.998 8.417 5.903 16.512 5.374 18.626 7.488 18.097z" />
-						<path d="M4.03,15.758l-1,4c-0.086,0.341,0.015,0.701,0.263,0.949C3.482,20.896,3.738,21,4,21c0.081,0,0.162-0.01,0.242-0.03l4-1 c0.176-0.044,0.337-0.135,0.465-0.263l8.292-8.292l1.294,1.292l1.414-1.414l-1.294-1.292L21,7.414 c0.378-0.378,0.586-0.88,0.586-1.414S21.378,4.964,21,4.586L19.414,3c-0.756-0.756-2.072-0.756-2.828,0l-2.589,2.589l-1.298-1.296 l-1.414,1.414l1.298,1.296l-8.29,8.29C4.165,15.421,4.074,15.582,4.03,15.758z M5.903,16.512l8.095-8.095l1.586,1.584 l-8.096,8.096l-2.114,0.529L5.903,16.512z" />
+					<svg
+						className="w-6 h-6 fill-current stroke-black"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="black"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth={2}
+							d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+						/>
 					</svg>
 				</button>
 				{isOpen && (
