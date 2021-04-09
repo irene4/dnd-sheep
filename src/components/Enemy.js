94a4bab236e29b894eb46d35354b3f17ed2a372e
irene4 <iallman@ufl.edu>
2021-04-08 21:22:35 -0400

    Enemy mobile layout improved

--- src/components/Enemy.js
+++ src/components/Enemy.js
@@ -31,26 +31,31 @@ function Enemy({ data, updateField }) {
 					onChange={update}
 				/>
 			</Tooltip>
-			<span className="pl-1 pr-2 bg-white rounded">
+			<span className="pl-1 pr-2 mx-2 bg-white rounded">
 				<Dice data={data.dice || ''} update={update} updateWithoutEvent={updateWithoutEvent} />
 			</span>
-			<button className="absolute text-sm text-red-700 hover:text-red-800 top-2 right-2" onClick={() => updateField(data.id, 'active', false)}>
-				<svg className="w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-					<path
-						fillRule="evenodd"
-						d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-						clipRule="evenodd"
-					/>
-				</svg>
-			</button>
-			<input
-				id="initiative"
-				value={data.initiative}
-				type="text"
-				className="absolute text-xs text-center text-red-700 placeholder-red-700 bg-transparent h-4 w-4 top-6 right-1 rounded"
-				placeholder="#"
-				onChange={update}
-			></input>
+			<div className="flex flex-row-reverse -mr-2">
+				<button
+					className="sm:absolute text-sm text-red-700 hover:text-red-800 sm:top-2 sm:right-2"
+					onClick={() => updateField(data.id, 'active', false)}
+				>
+					<svg className="w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+						<path
+							fillRule="evenodd"
+							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</button>
+				<input
+					id="initiative"
+					value={data.initiative}
+					type="text"
+					className="sm:absolute text-xs text-center text-red-700 placeholder-red-700 bg-transparent h-4 w-4 sm:top-6 sm:right-1 rounded"
+					placeholder="#"
+					onChange={update}
+				></input>
+			</div>
 		</div>
 	);
 }
