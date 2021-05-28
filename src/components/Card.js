c4f5eadafc63f2910ef14a47cac7a1d013346a0a
irene4 <iallman@ufl.edu>
2021-05-27 23:28:38 -0700

    hit dice value

--- src/components/Card.js
+++ src/components/Card.js
@@ -95,7 +95,7 @@ function Card({ data, updateField }) {
 					</span>
 					<span className="flex items-center text-xxxs">
 						<span className="text-gray-500 font-bold tracking-wider">Hit Dice</span>
-						<select>
+						<select value={data.hitDice}>
 							<option>d12</option>
 							<option>d10</option>
 							<option>d8</option>
